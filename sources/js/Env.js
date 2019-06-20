055667573834ff250c7e24643c9d4744cc490a63
Wiktor <wiktorjb@gmail.com>
2019-06-20 21:10:27 +0200

    Touch device detection

--- sources/js/Env.js
+++ sources/js/Env.js
@@ -6,6 +6,9 @@ var Env = {
     lang: document.documentElement.getAttribute("lang"),
 
     browser: (navigator.userAgent.indexOf("Edge") >= 0
-            || navigator.userAgent.indexOf("MSIE") >= 0) ? "ms" : "base"
+            || navigator.userAgent.indexOf("MSIE") >= 0) ? "ms" : "std",
+
+    device: (typeof window.ontouchstart !== "undefined"
+            || navigator.maxTouchPoints > 0) ? "touch" : "std"
 
 };
